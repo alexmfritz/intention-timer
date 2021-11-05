d331408ed6a1024f479071f7d67b1233cf8a632f
Devon Wigle <devonwigle@gmail.com>
2021-11-04 20:04:05 -0500

    Add helper functions and event listener to show timer

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 var activityPresenter = document.querySelector('.activity-presenter');
 var activityTitle = document.querySelector('#new-activity');
+var selectorBox = document.querySelector('.selector-box');
 //buttons
 var studyButton = document.querySelector('.study');
 var meditateButton = document.querySelector('.meditate');
 var exerciseButton = document.querySelector('.exercise');
 var startActivityButton = document.querySelector('#start-activity');
+var timerView = document.querySelector('.timer-presenter');
+var userActivityInputView = document.querySelector('.user-activity-input');
 //inputs
 var userAccomplishInput = document.querySelector('#user-accomplish');
 var userMinutesInput = document.querySelector('#minutes');
@@ -14,3 +17,35 @@ var selectionError = document.querySelector('.selection');
 var accomplishError = document.querySelector('.accomplish');
 var minutesError = document.querySelector('.minutes');
 var secondsError = document.querySelector('.seconds');
+
+//event listeners
+
+startActivityButton.addEventListener('click', showTimer);
+
+//functions
+
+function showTimer(element) {
+  show(timerView);
+  hide(userActivityInputView);
+  changeActivityTitle();
+};
+
+function changeActivityTitle() {
+  activityTitle.innerText = 'Current Activity';
+};
+
+function show(element) {
+  element.classList.remove('hidden');
+};
+
+function hide(element) {
+  element.classList.add('hidden');
+};
+
+function visible(element) {
+  element.classList.remove('visibility');
+};
+
+function invisible(element) {
+  element.classList.add('visibility');
+};
