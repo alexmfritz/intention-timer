32e815fad3f746c0bfedb12b627481f0f4aa7a7d
Devon Wigle <devonwigle@gmail.com>
2021-11-07 21:09:12 -0600

    Set timer to refresh without load

--- src/Activity.js
+++ src/Activity.js
@@ -9,23 +9,24 @@ class Activity {
     this.id = Date.now();
   }
   startTimer(minutes, seconds) {
-   disableButton(startTimerButton);
-   setInterval(function () {
-     if(seconds > 0) {
-       seconds -= 1;
-     } else if (minutes > 0) {
-       minutes -= 1;
-       seconds = 59;
-     } else if (minutes == 0 && seconds == 0) {
-       show(startTimerButton, 'disabled');
-       startTimerButton.innerText = 'FINISHED!';
-       show(logActivityButton, 'visibility');
-       return timerDisplay.innerText = 'NICE JOB!';
-     }
-     minutes = minutes.toString().padStart(2, '0');
-     seconds = seconds.toString().padStart(2, '0');
-     timerDisplay.innerText = `${minutes}:${seconds}`}, 1000
-   )
+    disableButton(startTimerButton);
+    var timer = setInterval(function () {
+      if(seconds > 0) {
+        seconds -= 1;
+      } else if (minutes > 0) {
+        minutes -= 1;
+        seconds = 59;
+      } else {
+        clearInterval(timer);
+        show(startTimerButton, 'disabled');
+        startTimerButton.innerText = 'FINISHED!';
+        show(logActivityButton, 'visibility');
+        return timerDisplay.innerText = 'NICE JOB!';
+      }
+      minutes = minutes.toString().padStart(2, '0');
+      seconds = seconds.toString().padStart(2, '0');
+      timerDisplay.innerText = `${minutes}:${seconds}`}, 1000
+    )
   }
   markComplete() {
     this.completed = true;
