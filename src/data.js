eafcbeb22d31a22f251cf2fecb8f033b4d5da27f
Stevie Ambroise <s.ambroise@protonmail.com>
2021-11-08 14:18:21 -0700

    Change class names to be more specific and created file for miscellaneous variables

---
+++ src/data.js
@@ -0,0 +1,10 @@
+//miscellaneous variables
+var keyErrors = ['-', '+', 'e', 'E', '.'];
+
+var meditateIds = ['meditate', 'meditateImg', 'meditateText'];
+
+var exerciseIds = ['exercise', 'exerciseImg', 'exerciseText'];
+
+var studyIds = ['study', 'studyImg', 'studyText'];
+
+var selectedCategory= '';
